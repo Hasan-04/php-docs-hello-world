60e9b23f0fd345c4a869c62b7b0135a74fa68a37
Hasan-04 <162113882+Hasan-04@users.noreply.github.com>
2024-03-04 12:54:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,70 +12,10 @@
             background-size: cover; /* To cover the whole background */
             background-position: center; /* To center the image */
         }
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 10px 0;
-            text-align: center;
-        }
-        header h1 {
-            margin: 0;
-        }
-        main {
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-            padding: 20px;
-        }
-        main div {
-            flex: 1;
-            text-align: center;
-        }
-        main div h2 {
-            margin: 10px 0;
-        }
-        main div p {
-            margin: 0;
-        }
-        footer {
-            background-color: #333;
-            color: #fff;
-            padding: 10px 0;
-            text-align: center;
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-        }
+        /* Other style rules... */
     </style>
 </head>
 <body>
-    <header>
-        <h1>Website</h1>
-    </header>
-    <main>
-        <div>
-            <h2>Login</h2>
-            <form action="login.php" method="post">
-                <label for="username">Username:</label><br>
-                <input type="text" id="username" name="username"><br>
-                <label for="password">Password:</label><br>
-                <input type="password" id="password" name="password"><br>
-                <input type="submit" value="Login">
-            </form>
-        </div>
-        <div>
-            <h2>Sign Up</h2>
-            <form action="signup.php" method="post">
-                <label for="username">Username:</label><br>
-                <input type="text" id="username" name="username"><br>
-                <label for="password">Password:</label><br>
-                <input type="password" id="password" name="password"><br>
-                <input type="submit" value="Sign Up">
-            </form>
-        </div>
-    </main>
-    <footer>
-        &copy; 2023 Website
-    </footer>
+    <!-- Body content... -->
 </body>
 </html>
