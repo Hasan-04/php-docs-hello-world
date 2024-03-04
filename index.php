2dab691745f6d2e84218a1e0b92cd296c13a59f9
Hasan-04 <162113882+Hasan-04@users.noreply.github.com>
2024-03-04 13:11:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <h1>  hassan - youness -salim </h1>
-    <title> LOGIN </title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-image: url('https://example.com/path/to/your/image.jpg'); /* Replace with your image URL or path */
-            background-size: cover; /* To cover the whole background */
-            background-position: center; /* To center the image */
-        }
-        /* Other style rules... */
-    </style>
+    <h1>youness hassan salim </h1>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta charset="UTF-8">
+  <title>NESSYOU</title>
+  <link rel="stylesheet" href="//unpkg.com/docsify/themes/vue.css">
 </head>
 <body>
-    <!-- Body content... -->
+  <noscript>This page requires JavaScript to work, please enable it or just read it on <a href="https://github.com/sdil/awesome-production-open-projects">Here</a>.</noscript>
+  <div id="app">Loading...</div>
+  <script>
+    window.$docsify = {
+      name: 'Open Production-ready Web Projects',
+      repo: 'sdil/open-production-web-projects',
+      search: ['/']
+    }
+  </script>
+  <script src="//unpkg.com/docsify/lib/docsify.min.js"></script>
+  <script src="//unpkg.com/docsify/lib/plugins/search.min.js"></script>
 </body>
 </html>
